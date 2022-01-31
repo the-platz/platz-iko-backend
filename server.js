47c570e6afcb037c5fc88b53797b747edeebcdbd
Phat Nguyen Luu <yadsendew@protonmail.com>
2022-01-31 09:58:55 +0000

    refactor: get only transaction

--- server.js
+++ server.js
@@ -6,8 +6,19 @@ let port = process.env.PLATZ_IKO_BACKEND_PORT || 5000;
 let connectionString = process.env.INDEXER_DB_CONN;
 console.log(connectionString)
 
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  next();
+});
+
 app.get('/donation_history', function (req, res) {
     let account_id = req.query.account_id
+    let limit = parseInt(req.query.limit)
+    let offset = parseInt(req.query.offset)
+
     console.log(account_id)
     const pool = new Pool({
       connectionString,
@@ -16,13 +27,12 @@ app.get('/donation_history', function (req, res) {
     const donation_history_query = {
       name: 'donation_history_query',
       text: `
-        SELECT t.transaction_hash, t.block_timestamp, ta.args -> 'deposit' as donation_amount, r.receipt_id, eo.status 
+        SELECT t.transaction_hash, t.receiver_account_id, t.block_timestamp, ta.args -> 'deposit' as donation_amount
         FROM public.transactions t
         inner join public.transaction_actions ta on t.transaction_hash = ta.transaction_hash
-        right join public.receipts r  on r.originated_from_transaction_hash = t.transaction_hash
-        inner join public.execution_outcomes eo on eo.receipt_id = r.receipt_id
-        where t.signer_account_id = $1 and ta.args -> 'method_name' = '"donate"';`,
-      values: [account_id]
+        where t.signer_account_id = $1 and ta.args -> 'method_name' = '"donate"'
+        limit $2 offset $3;`,
+      values: [account_id, limit, offset]
     }
 
     pool.query(donation_history_query, (err, result) => {
@@ -34,31 +44,10 @@ app.get('/donation_history', function (req, res) {
 
         return
       }
-      let transaction_map  = new Map();
-
-      result.rows.forEach((row, _, __) => {
-        const tx_hash = row.transaction_hash
-        let tx_info = transaction_map.get(tx_hash)
-
-        if (!tx_info) {
-          tx_info = {
-            transaction_hash: tx_hash,
-            block_timestamp: row.block_timestamp,
-            donation_amount: row.donation_amount,
-            tx_succeeded: true, // default
-          }
-        }
-
-        if (row.status == 'FAILURE') {
-          tx_info.tx_succeeded = false
-        }
-
-        transaction_map.set(tx_hash, tx_info)
-      })
 
       res.json({
         success: true,
-        data: Array.from(transaction_map.values())
+        data: result.rows
       })
       pool.end()
     })
