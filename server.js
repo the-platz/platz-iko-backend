ed9d69f452c02baeb0d81c36f64ad7dde62bb956
Phat Nguyen Luu <yadsendew@protonmail.com>
2022-02-07 11:37:04 +0000

    feat: add tx status query

--- server.js
+++ server.js
@@ -14,47 +14,15 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.get('/transaction/receipts', function (req, res) {
-    let tx_hash = req.query.tx_hash
-
-    const pool = new Pool({
-      connectionString,
-    })
-
-    const tx_status_query = {
-      name: 'tx_status_query',
-      text: `SELECT t.transaction_hash, r.receipt_id , eo.status
-      FROM public.transactions t
-      right join public.receipts r  on r.originated_from_transaction_hash = t.transaction_hash
-      inner join public.execution_outcomes eo on eo.receipt_id = r.receipt_id
-      where transaction_hash = $1;`,
-      values: [tx_hash]
-    }
-
-    pool.query(tx_status_query, (err2, result) => {
-      if (err2) {
-        res.status(500).json({
-          success: false,
-          data: err2
-        })
-
-        return
-      }
-
-      res.status(200).json({
-        success: true,
-        data: result.rows
-      })
-      pool.end()
-    })
-})
-
-app.get('/transaction/donation', function (req, res) {
-    let account_id = req.query.account_id
+app.get('/transactions/donation', function (req, res) {
+    let sender_account_id_filter = req.query.account_id
+    // TODO: env var
+    let receiver_account_id_filter = `%${process.env.IKO_MASTER_ACCOUNT_ID}`
+    let tx_status_filter = 
+      req.query.includeFailedTxs == 'true' ? '%SUCCESS_VALUE' : 'SUCCESS_VALUE'
     let limit = parseInt(req.query.limit)
     let offset = parseInt(req.query.offset)
 
-    console.log(account_id)
     const pool = new Pool({
       connectionString,
     })
@@ -62,12 +30,37 @@ app.get('/transaction/donation', function (req, res) {
     const donation_history_query = {
       name: 'donation_history_query',
       text: `
-        SELECT t.transaction_hash, t.receiver_account_id, t.block_timestamp, ta.args -> 'deposit' as donation_amount
-        FROM public.transactions t
-        inner join public.transaction_actions ta on t.transaction_hash = ta.transaction_hash
-        where t.signer_account_id = $1 and ta.args -> 'method_name' = '"donate"'
-        limit $2 offset $3;`,
-      values: [account_id, limit, offset]
+      select txs.transaction_hash,
+        txs.receiver_account_id,
+        txs.block_timestamp,
+        txs.statuses,
+        case when txs.statuses like 'FAILURE%' 
+        then true else false
+        end as is_failed
+      from (
+        select t_sub.transaction_hash, 
+          (array_agg(t_sub.receiver_account_id))[1] as receiver_account_id, 
+          (array_agg(t_sub.block_timestamp))[1] as block_timestamp,
+          string_agg(distinct eo.status::text, ',') as statuses
+        from public.transactions t_sub
+        inner join public.transaction_actions ta on t_sub.transaction_hash = ta.transaction_hash
+        right join public.receipts r  on r.originated_from_transaction_hash = t_sub.transaction_hash
+        inner join public.execution_outcomes eo on eo.receipt_id = r.receipt_id
+        where t_sub.signer_account_id = $1
+          and t_sub.receiver_account_id like $2
+          and ta.args -> 'method_name' = '"donate"'
+        group by t_sub.transaction_hash, t_sub.receiver_account_id, t_sub.block_timestamp
+        having string_agg(distinct eo.status::text, ',') like $3
+        ) as txs
+      order by txs.block_timestamp desc
+      limit $4 offset $5;`,
+      values: [
+        sender_account_id_filter,
+        receiver_account_id_filter,
+        tx_status_filter,
+        limit,
+        offset
+      ]
     }
 
     pool.query(donation_history_query, (err, result) => {
